7d455604742147c5829a0c395aa16404fb4089c8
rohith200 <42884535+rohith200@users.noreply.github.com>
2021-12-15 12:20:07 +0530

    hi

--- src/main/java/com/houarizegai/calculator/Calculator.java
+++ src/main/java/com/houarizegai/calculator/Calculator.java
@@ -31,42 +31,7 @@ public class Calculator {
     private boolean addWrite = true; // Connect numbers in display
     private double val = 0; // Save the value typed for calculation
 
-    /*
-        Mx Calculator: 
-        X = Row
-        Y = Column
     
-        +-------------------+
-        |   +-----------+   |   y[0]
-        |   |           |   |
-        |   +-----------+   |
-        |                   |
-        |   C  <-   %   /   |   y[1]
-        |   7   8   9   *   |   y[2]
-        |   4   5   6   -   |   y[3]
-        |   1   2   3   +   |   y[4]
-        |   .   0     =     |   y[5]
-        +-------------------+
-         x[0] x[1] x[2] x[3]
-    
-    */
-    
-    /*    
-        +-------------------+
-        |   +-----------+   |   y[0]
-        |   |           |   |
-        |   +-----------+   |
-        |                   |
-        |   0   1   1   3   |   y[1]
-        |   4   5   6   7   |   y[2]
-        |   8   9   10  11  |   y[3]
-        |   12  13  14  15  |   y[4]
-        |   16  17    18    |   y[5]
-        +-------------------+
-         x[0] x[1] x[2] x[3]
-    
-    */
-
     public Calculator() {
         window = new JFrame("Calculator");
         window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
